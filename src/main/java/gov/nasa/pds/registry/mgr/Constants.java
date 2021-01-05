c6dad72a068e31db87babdf60efa9e8f1dc65b75
Eugene <test@test.com>
2021-01-05 11:09:04 -0800

    Changed field names to <ns:class/ns:attribute> in registry index and default data dictionary

--- src/main/java/gov/nasa/pds/registry/mgr/Constants.java
+++ src/main/java/gov/nasa/pds/registry/mgr/Constants.java
@@ -2,10 +2,14 @@ package gov.nasa.pds.registry.mgr;
 
 public interface Constants
 {
+    // Indices
     public static final String DEFAULT_REGISTRY_INDEX = "registry";
     public static final String REGISTRY_DD_INDEX = "registry_dd";
-    
+
+    // Authentication
     public static final String AUTH_TRUST_SELF_SIGNED = "trust.self-signed";
     
-    public static final String BLOB_FIELD = "ops/Label_File_Info/ops/blob";
+    // Fields
+    public static final String NS_SEPARATOR = ":";
+    public static final String BLOB_FIELD = "ops:Label_File_Info/ops:blob";
 }
