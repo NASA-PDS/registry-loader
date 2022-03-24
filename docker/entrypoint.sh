7f530a814fc92d4323e44ec25f136a328f48bedf
Thomas Loubrieu <loubrieu@jpl.nasa.gov>
2022-03-24 11:14:47 -0700

    add step to change the status of data in registry so that is it visible by default in the API

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -57,3 +57,5 @@ harvest -c "$HARVEST_CFG_FILE"
 # Load data into Elasticsearch
 echo "Loading harvested data into Elasticsearch ..." 1>&2
 registry-manager load-data -dir /tmp/harvest/out/ -es "$ES_URL" -force -auth /etc/es-auth.cfg
+registry-manager set-archive-status -status archived -lidvid urn:nasa:pds:insight_rad::2.1 -es "$ES_URL" -auth /etc/es-auth.cfg
+
