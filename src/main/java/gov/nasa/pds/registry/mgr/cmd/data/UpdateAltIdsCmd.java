15655d8b8af408c94fad27b565560f949ffabce8
Eugene <tumbao72@yahoo.com>
2022-04-06 17:15:15 -0700

    update-alt-ids command

--- src/main/java/gov/nasa/pds/registry/mgr/cmd/data/UpdateAltIdsCmd.java
+++ src/main/java/gov/nasa/pds/registry/mgr/cmd/data/UpdateAltIdsCmd.java
@@ -1,7 +1,14 @@
 package gov.nasa.pds.registry.mgr.cmd.data;
 
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+
 import org.apache.commons.cli.CommandLine;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.elasticsearch.client.ResponseException;
 import org.elasticsearch.client.RestClient;
 
@@ -19,7 +26,6 @@ import gov.nasa.pds.registry.mgr.cmd.CliCommand;
  */
 public class UpdateAltIdsCmd implements CliCommand
 {
-
     
     /**
      * Constructor
@@ -42,12 +48,19 @@ public class UpdateAltIdsCmd implements CliCommand
         String indexName = cmdLine.getOptionValue("index", Constants.DEFAULT_REGISTRY_INDEX);
         String authPath = cmdLine.getOptionValue("auth");
 
+        String filePath = cmdLine.getOptionValue("file");
+        if(filePath == null) throw new Exception("Missing required parameter '-file'");
+        File file = new File(filePath);
+        if(!file.exists()) throw new Exception("Input file doesn't exist: " + file.getAbsolutePath());
+        
         RestClient client = null;
         
         try
         {
             client = EsClientFactory.createRestClient(esUrl, authPath);
 
+            updateIds(client, file);
+            
         }
         catch(ResponseException ex)
         {
@@ -59,6 +72,59 @@ public class UpdateAltIdsCmd implements CliCommand
         }
     }
 
+    
+    private void updateIds(RestClient client, File file) throws Exception
+    {
+        BufferedReader rd = null;
+        Logger log = LogManager.getLogger(this.getClass());
+
+        try
+        {
+            rd = new BufferedReader(new FileReader(file));
+            
+            String line;
+            int lineNum = 0;
+            
+            while((line = rd.readLine()) != null)
+            {
+                lineNum++;
+                line = line.trim();
+                
+                String[] ids = StringUtils.split(line, ",;\t ");
+                if(ids == null || ids.length != 2)
+                {
+                    log.warn("Line " + lineNum + " has invalid value: [" + line + "]");
+                    continue;
+                }
+                
+                if(!ids[0].contains("::"))
+                {
+                    log.warn("Line " + lineNum + " has invalid LIDVID: [" + ids[0] + "]");
+                    continue;
+                }
+                
+                if(!ids[1].contains("::"))
+                {
+                    log.warn("Line " + lineNum + " has invalid LIDVID: [" + ids[1] + "]");
+                    continue;
+                }
+
+                updateIds(client, ids);
+            }
+        }
+        finally
+        {
+            CloseUtils.close(rd);
+        }
+    }
+    
+    
+    private void updateIds(RestClient client, String[] lidvids) throws Exception
+    {
+        System.out.println(lidvids[0]);
+        System.out.println(lidvids[1]);
+    }
+    
     
     public void printHelp()
     {
