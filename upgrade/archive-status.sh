4e4ebfe9b6bd52f84f9b1d3d410f0a8f3ef1fe93
Eugene <test@test.com>
2022-05-04 10:29:38 -0700

    Upgrade archive status

--- upgrade/archive-status.sh
+++ upgrade/archive-status.sh
@@ -2,7 +2,7 @@
 
 update_schema() 
 {
-curl -k -X PUT "$elastic_url/registry/_mapping" -H 'Content-Type: application/json' -d'
+curl -k $elastic_auth -X PUT "$elastic_url/registry/_mapping" -H 'Content-Type: application/json' -d'
 {
   "properties": {
     "ops:Tracking_Meta/ops:archive_status": { "type": "keyword" }
@@ -13,7 +13,7 @@ curl -k -X PUT "$elastic_url/registry/_mapping" -H 'Content-Type: application/js
 
 set_archive_status()
 {
-curl -k -X POST "$elastic_url/registry/_update_by_query" -H 'Content-Type: application/json' -d'
+curl -k $elastic_auth -X POST "$elastic_url/registry/_update_by_query" -H 'Content-Type: application/json' -d'
 {
   "script" : {
     "source": "ctx._source['params.field'] = params.value",
@@ -32,7 +32,12 @@ if [[ $# -eq 1 ]]; then
     elastic_url=$1
     update_schema
     set_archive_status
+elif [[ $# -eq 3 ]]; then
+    elastic_url=$1
+    elastic_auth="-u $2:$3"
+    update_schema
+    set_archive_status
 else
-    echo "Usage: $(basename $BASH_SOURCE) <OpenSearch URL> " >&2
+    echo "Usage: $(basename $BASH_SOURCE) <OpenSearch URL> [<username> <password>]" >&2
     exit 2
 fi
