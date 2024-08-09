30c2c5ba48833bd4f5f963a0ecefcae8bf3bd924
al-niessner <1130658+al-niessner@users.noreply.github.com>
2024-08-09 10:20:31 -0700

    New feature to update archive status via packageId (#82)
    * use the common archive checker
    
    * allow changing status by package id
    
    * Update SetArchiveStatusCmd.java
    
    Must have missed this edit.
    
    * doh! - workaround
    
    This will keep you working for a while but this really needs to be changed to a scroll or done in pages that match the batch size. Probably really need to do scroll then page it out based on bulk limits.
    
    ---------
    
    Co-authored-by: Al Niessner <Al.Niessner@xxx.xxx>

--- src/main/java/gov/nasa/pds/registry/mgr/cmd/data/SetArchiveStatusCmd.java
+++ src/main/java/gov/nasa/pds/registry/mgr/cmd/data/SetArchiveStatusCmd.java
@@ -1,8 +1,6 @@
 package gov.nasa.pds.registry.mgr.cmd.data;
 
-import java.util.Set;
-import java.util.TreeSet;
-
+import java.util.Arrays;
 import org.apache.commons.cli.CommandLine;
 import gov.nasa.pds.registry.common.ConnectionFactory;
 import gov.nasa.pds.registry.common.EstablishConnectionFactory;
@@ -10,6 +8,7 @@ import gov.nasa.pds.registry.common.ResponseException;
 import gov.nasa.pds.registry.common.RestClient;
 import gov.nasa.pds.registry.common.es.dao.ProductDao;
 import gov.nasa.pds.registry.common.es.service.ProductService;
+import gov.nasa.pds.registry.common.util.ArchiveStatus;
 import gov.nasa.pds.registry.common.util.CloseUtils;
 import gov.nasa.pds.registry.mgr.cmd.CliCommand;
 
@@ -21,17 +20,11 @@ import gov.nasa.pds.registry.mgr.cmd.CliCommand;
  * @author karpenko
  */
 public class SetArchiveStatusCmd implements CliCommand {
-  private Set<String> statusNames;
-
+  private ArchiveStatus archiveStatusUtils = new ArchiveStatus();
   /**
    * Constructor
    */
   public SetArchiveStatusCmd() {
-    statusNames = new TreeSet<>();
-    statusNames.add("staged");
-    statusNames.add("archived");
-    statusNames.add("certified");
-    statusNames.add("restricted");
   }
 
     @Override
@@ -48,9 +41,9 @@ public class SetArchiveStatusCmd implements CliCommand {
         
         String status = getStatus(cmdLine);
 
-        String lidvid = cmdLine.getOptionValue("lidvid");
-        if(lidvid == null) throw new Exception("Missing required parameter '-lidvid'");
-        
+        String lidvid = cmdLine.getOptionValue("lidvid"), pid = cmdLine.getOptionValue("packageId");
+        if(lidvid == null && pid == null) throw new Exception("Missing required parameter '-lidvid' or '-packageId");
+        if (lidvid != null && pid != null) throw new Exception("Specify just one of '-lidvid' or '-packageId'");
         RestClient client = null;
         
         try
@@ -61,7 +54,14 @@ public class SetArchiveStatusCmd implements CliCommand {
             ProductDao dao = new ProductDao(client, conFact.getIndexName());
             ProductService srv = new ProductService(dao);
             
-            srv.updateArchveStatus(lidvid, status);
+            if (pid == null) srv.updateArchiveStatus(lidvid, status);
+            if (lidvid ==  null) {
+              srv.updateArchiveStatus (client.performRequest(client.createSearchRequest()
+                  .setIndex(conFact.getIndexName())
+                  .buildTermQuery("_package_id", pid)
+                  .setReturnedFields(Arrays.asList("lidvid"))
+                  .setSize(1000)).lidvids(), status); // this breaks if really huge need to use scroll
+            }
         }
         catch(ResponseException ex)
         {
@@ -88,14 +88,9 @@ public class SetArchiveStatusCmd implements CliCommand {
     }
 
     String status = tmp.toLowerCase();
-    if (!statusNames.contains(status)) {
-      String authorized_status = String.join(", ", this.statusNames);
-      throw new Exception("Invalid '-status' parameter value: '" + tmp + "'. Authorized values are "
-          + authorized_status + ".");
-    }
+    this.archiveStatusUtils.validateStatusName(status);
     return status;
   }
-
 
   /**
    * Print help screen
@@ -109,7 +104,7 @@ public class SetArchiveStatusCmd implements CliCommand {
     System.out.println("Required parameters:");
     System.out.println("  -status <status>   One of the following values:");
 
-    for (String name : statusNames) {
+    for (String name : this.archiveStatusUtils.statusNames) {
       System.out.println("     " + name);
     }
 
