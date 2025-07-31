f0153c7595a28b104c76e6754c34d134e94ca9fd
Al Niessner <Al.Niessner@xxx.xxx>
2025-07-31 14:26:27 -0700

    fixed up LDD errors

--- src/main/java/gov/nasa/pds/registry/common/dd/DDNJsonWriter.java
+++ src/main/java/gov/nasa/pds/registry/common/dd/DDNJsonWriter.java
@@ -1,9 +1,12 @@
 package gov.nasa.pds.registry.common.dd;
 
 import java.io.File;
-import com.google.gson.stream.JsonWriter;
-
-import gov.nasa.pds.registry.common.util.json.BaseNJsonWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import javax.annotation.Nonnull;
+import gov.nasa.pds.registry.common.util.json.Serializer;
 
 
 /**
@@ -12,48 +15,59 @@ import gov.nasa.pds.registry.common.util.json.BaseNJsonWriter;
  * @author karpenko
  *
  */
-public class DDNJsonWriter extends BaseNJsonWriter<DDRecord>
-{
-    /**
-     * Constructor
-     * @param file output file
-     * @throws Exception an exception
-     */
-    public DDNJsonWriter(File file, boolean overwrite) throws Exception
-    {
-        super(file, overwrite ? "index" : "create");
+public class DDNJsonWriter {
+  private boolean firstWrite = true;
+  public final File file;
+  public final String action;
+  /**
+   * Constructor
+   * 
+   * @param file output file
+   * @throws Exception an exception
+   */
+  public DDNJsonWriter(File file, boolean overwrite) throws Exception {
+    this.action = overwrite ? "index" : "create";
+    this.file = file;
+  }
+
+
+  /**
+   * Write one data record.
+   */
+
+  private HashMap<String,String> dataRecord2Doc(DDRecord data) {
+    HashMap<String,String> document = new HashMap<String,String>();
+    String fieldName =
+        (data.esFieldName != null) ? data.esFieldName : data.esFieldNameFromComponents();
+    document.put("es_field_name", fieldName);
+    document.put("es_data_type", data.esDataType);
+    document.put("class_ns", data.classNs);
+    document.put("class_name", data.className);
+    document.put("attr_ns", data.attrNs);
+    document.put("attr_name", data.attrName);
+    document.put("data_type", data.dataType);
+    document.put("description", data.description);
+    document.put("im_version", data.imVersion);
+    document.put("ldd_version", data.lddVersion);
+    document.put("date", data.date);
+    return document;
+  }
+
+  public void write(@Nonnull String pk, @Nonnull DDRecord data) throws IOException {
+    this.write(pk, data, this.action);
+  }
+  public void write(@Nonnull String pk, @Nonnull DDRecord data, String action) throws IOException {
+    HashMap<String,HashMap<String,String>> command = new HashMap<String,HashMap<String,String>>();
+    Serializer serializer = new Serializer(false);
+    command.put(this.action, new HashMap<String,String>());
+    command.get(this.action).put("_id", pk);
+    try (FileWriter file = new FileWriter(this.file, !this.firstWrite)) {
+      for (String line : serializer.asBulkPairs(Arrays.asList(serializer.new Pair(command, dataRecord2Doc(data))))) {
+        file.write (line);
+        file.write ("\n");
+      }
+      file.close();
+      this.firstWrite = false;
     }
-
-    
-    /**
-     * Write one data record.
-     */
-    @Override
-    public void writeDataRecord(JsonWriter jw, DDRecord data) throws Exception
-    {
-        String fieldName = (data.esFieldName != null) ? data.esFieldName : data.esFieldNameFromComponents();
-        writeField(jw, "es_field_name", fieldName);
-        
-        writeField(jw, "es_data_type", data.esDataType);
-
-        writeField(jw, "class_ns", data.classNs);
-        writeField(jw, "class_name", data.className);
-        
-        writeField(jw, "attr_ns", data.attrNs);
-        writeField(jw, "attr_name", data.attrName);
-        
-        writeField(jw, "data_type", data.dataType);
-        writeField(jw, "description", data.description);
-
-        writeField(jw, "im_version", data.imVersion);
-        writeField(jw, "ldd_version", data.lddVersion);
-        writeField(jw, "date", data.date);
-    }
-
-
-    private void writeField(JsonWriter jw, String name, String value) throws Exception
-    {
-        if(value == null) return;
-        jw.name(name).value(value);
-    }
+  }
 }
