f105d57c1b2f073cd9c84cdba1677ef13ec62823
Al Niessner <Al.Niessner@xxx.xxx>
2025-08-14 13:06:57 -0700

    fix a bunch of sonarque finds

--- src/main/java/gov/nasa/pds/registry/common/meta/CollectionMetadataExtractor.java
+++ src/main/java/gov/nasa/pds/registry/common/meta/CollectionMetadataExtractor.java
@@ -3,6 +3,7 @@ package gov.nasa.pds.registry.common.meta;
 import java.util.Set;
 
 import javax.xml.xpath.XPathExpression;
+import javax.xml.xpath.XPathExpressionException;
 import javax.xml.xpath.XPathFactory;
 
 import org.w3c.dom.Document;
@@ -21,9 +22,10 @@ public class CollectionMetadataExtractor
 
     /**
      * Constructor
+     * @throws XPathExpressionException 
      * @throws Exception an exception
      */
-    public CollectionMetadataExtractor() throws Exception
+    public CollectionMetadataExtractor() throws XPathExpressionException
     {
         XPathFactory xpf = XPathFactory.newInstance();
         xFileName = XPathUtils.compileXPath(xpf, "//*[local-name() = 'File_Area_Inventory']/*[local-name() = 'File']/*[local-name() = 'file_name']");
@@ -34,9 +36,10 @@ public class CollectionMetadataExtractor
      * Extract collection inventory file names
      * @param doc Parsed collection label (XML DOM)
      * @return a set of files (usually there is only one inventory file)
+     * @throws XPathExpressionException 
      * @throws Exception an exception
      */
-    public Set<String> extractInventoryFileNames(Document doc) throws Exception
+    public Set<String> extractInventoryFileNames(Document doc) throws XPathExpressionException
     {
         return XPathUtils.getStringSet(doc, xFileName);
     }
