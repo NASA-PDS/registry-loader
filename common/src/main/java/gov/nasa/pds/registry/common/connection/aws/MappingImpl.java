0d0975024ef6655cba90cf25ff86916f06abde7d
thomas loubrieu <thomas.loubrieu@jpl.nasa.gov>
2025-11-18 17:03:35 -0800

    misc

--- common/src/main/java/gov/nasa/pds/registry/common/connection/aws/MappingImpl.java
+++ common/src/main/java/gov/nasa/pds/registry/common/connection/aws/MappingImpl.java
@@ -1,26 +1,38 @@
 package gov.nasa.pds.registry.common.connection.aws;
 
+
 import java.util.Collection;
 import java.util.HashMap;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.opensearch.client.opensearch._types.mapping.Property;
 import org.opensearch.client.opensearch.indices.GetMappingRequest;
 import org.opensearch.client.opensearch.indices.PutMappingRequest;
+
+
 import gov.nasa.pds.registry.common.Request.Mapping;
 import gov.nasa.pds.registry.common.util.Tuple;
 
 class MappingImpl implements Mapping {
+
+  static private Logger logger = LogManager.getLogger(MappingImpl.class);
+
+
   boolean isGet = true;
   final GetMappingRequest.Builder craftsman_get = new GetMappingRequest.Builder();
   final PutMappingRequest.Builder craftsman_set = new PutMappingRequest.Builder();
+
   @Override
   public Mapping buildUpdateFieldSchema(Collection<Tuple> pairs) {
-    HashMap<String,Property> mapping = new HashMap<String,Property>();
+    HashMap<String, Property> mapping = new HashMap<String, Property>();
     for (Tuple t : pairs) {
       Property.Builder journeyman = new Property.Builder();
       String fieldName = t.item1;
       String fieldType = t.item2;
+
       PropertyHelper.setType(journeyman, fieldType);
       mapping.put(fieldName, journeyman.build());
+
     }
     this.craftsman_set.properties(mapping);
     this.isGet = false;
