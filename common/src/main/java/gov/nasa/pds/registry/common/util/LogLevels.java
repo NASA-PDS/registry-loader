b83d5845e3097e805299a0241dd3a5197c7b2b49
Al Niessner <Al.Niessner@xxx.xxx>
2026-07-24 10:24:07 -0700

    make sonar happy

--- common/src/main/java/gov/nasa/pds/registry/common/util/LogLevels.java
+++ common/src/main/java/gov/nasa/pds/registry/common/util/LogLevels.java
@@ -3,6 +3,7 @@ package gov.nasa.pds.registry.common.util;
 import org.apache.logging.log4j.Level;
 
 public class LogLevels {
+  private LogLevels() {}
   public static final Level LABEL_FAILURE = Level.forName("LABEL FAILURE", 121); // batch did not accept lidvid
   public static final Level LABEL_IGNORED = Level.forName("LABEL IGNORED", 122); // file could not be read as a label
   public static final Level LABEL_MATCHED = Level.forName("LABEL MATCHED", 123); // batch rejected as duplicate lidvid
