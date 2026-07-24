2096c22c84fc63338f4776fb2b55cc70a9809751
Al Niessner <Al.Niessner@xxx.xxx>
2026-07-24 11:01:17 -0700

    make sonar happier

--- common/src/main/java/gov/nasa/pds/registry/common/util/log/Log4jConfigurator.java
+++ common/src/main/java/gov/nasa/pds/registry/common/util/log/Log4jConfigurator.java
@@ -17,80 +17,78 @@ import org.apache.logging.log4j.core.config.builder.impl.BuiltConfiguration;
  * 
  * @author karpenko
  */
-public class Log4jConfigurator
-{
-    /**
-     * Configure Log4j v2 logging library.
-     * @param verbosity Log verbosity value: 0 = ALL, 1 = INFO, 2 = WARN, 3 = ERROR
-     * @param filePath Log file path
-     */
-    public static void configure(String verbosity, @Nonnull String filePath) 
-    {
-        // Configure Log4j
-        ConfigurationBuilder<BuiltConfiguration> cfg = ConfigurationBuilderFactory.newConfigurationBuilder();
-        cfg.setStatusLevel(Level.ERROR);
-        cfg.setConfigurationName("Harvest");
-        
-        // Appenders
-        addConsoleAppender(cfg, "console");
-        addFileAppender(cfg, "file", filePath);
+public class Log4jConfigurator {
+  private Log4jConfigurator() {}
 
-        // Root logger
-        RootLoggerComponentBuilder rootLog = cfg.newRootLogger(Level.OFF);
-        rootLog.add(cfg.newAppenderRef("console"));
-        rootLog.add(cfg.newAppenderRef("file"));
-        cfg.add(rootLog);
-        
-        // Default Harvest logger
-        Level level = parseLogLevel(verbosity);
-        LoggerComponentBuilder defLog = cfg.newLogger("gov.nasa.pds", level);
-        cfg.add(defLog);
-        
-        // Init Log4j
-        Configurator.initialize(cfg.build());
-    }
-    
-    
-    private static void addConsoleAppender(ConfigurationBuilder<BuiltConfiguration> cfg, String name)
-    {
-        AppenderComponentBuilder appender = cfg.newAppender(name, "CONSOLE");
-        appender.addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
-        appender.add(cfg.newLayout("PatternLayout").addAttribute("pattern", "%d{yyyy-MM-dd HH:mm:ss} [%-5p] (%F:%M:%L) %m%n%throwable"));
-        cfg.add(appender);
-    }
-    
-    
-    private static void addFileAppender(ConfigurationBuilder<BuiltConfiguration> cfg, String name, @Nonnull String filePath)
-    {
-        AppenderComponentBuilder appender = cfg.newAppender(name, "FILE");
-        appender.addAttribute("fileName", filePath);
-        appender.addAttribute("append", false);
-        appender.add(cfg.newLayout("PatternLayout").addAttribute("pattern", "%d{yyyy-MM-dd HH:mm:ss} [%-5p] (%F:%M:%L) %m%n%throwable"));
-        cfg.add(appender);
-    }
-    
-    
-    private static Level parseLogLevel(String verbosity)
-    {
-        // Logger is not setup yet. Print to console.
-        if(verbosity == null)
-        {
-            System.out.println("[WARN] Log verbosity is not set. Will use 'INFO'.");
-            return Level.INFO;
-        }
-        
-        switch(verbosity.toUpperCase())
-        {
-        case "ALL": return Level.ALL;
-        case "DEBUG": return Level.DEBUG;
-        case "INFO": return Level.INFO;
-        case "WARN": return Level.WARN;
-        case "ERROR": return Level.ERROR;
-        }
+  /**
+   * Configure Log4j v2 logging library.
+   * 
+   * @param verbosity Log verbosity value: 0 = ALL, 1 = INFO, 2 = WARN, 3 = ERROR
+   * @param filePath Log file path
+   */
+  public static void configure(String verbosity, @Nonnull String filePath) {
+    // Configure Log4j
+    ConfigurationBuilder<BuiltConfiguration> cfg =
+        ConfigurationBuilderFactory.newConfigurationBuilder();
+    cfg.setStatusLevel(Level.ERROR);
+    cfg.setConfigurationName("Harvest");
 
-        // Logger is not setup yet. Print to console.
-        System.out.println("[WARNING] Invalid log verbosity '" + verbosity + "'. Will use 1 (Info).");
+    // Appenders
+    addConsoleAppender(cfg, "console");
+    addFileAppender(cfg, "file", filePath);
+
+    // Root logger
+    RootLoggerComponentBuilder rootLog = cfg.newRootLogger(Level.OFF);
+    rootLog.add(cfg.newAppenderRef("console"));
+    rootLog.add(cfg.newAppenderRef("file"));
+    cfg.add(rootLog);
+
+    // Default Harvest logger
+    Level level = parseLogLevel(verbosity);
+    LoggerComponentBuilder defLog = cfg.newLogger("gov.nasa.pds", level);
+    cfg.add(defLog);
+
+    // Init Log4j
+    Configurator.initialize(cfg.build());
+  }
+
+
+  private static void addConsoleAppender(ConfigurationBuilder<BuiltConfiguration> cfg,
+      String name) {
+    AppenderComponentBuilder appender = cfg.newAppender(name, "CONSOLE");
+    appender.addAttribute("target", ConsoleAppender.Target.SYSTEM_OUT);
+    appender.add(cfg.newLayout("PatternLayout").addAttribute("pattern",
+        "%d{yyyy-MM-dd HH:mm:ss} [%-5p] (%F:%M:%L) %m%n%throwable"));
+    cfg.add(appender);
+  }
+
+
+  private static void addFileAppender(ConfigurationBuilder<BuiltConfiguration> cfg, String name,
+      @Nonnull String filePath) {
+    AppenderComponentBuilder appender = cfg.newAppender(name, "FILE");
+    appender.addAttribute("fileName", filePath);
+    appender.addAttribute("append", false);
+    appender.add(cfg.newLayout("PatternLayout").addAttribute("pattern",
+        "%d{yyyy-MM-dd HH:mm:ss} [%-5p] (%F:%M:%L) %m%n%throwable"));
+    cfg.add(appender);
+  }
+
+
+  private static Level parseLogLevel(@Nonnull String verbosity) {
+    switch (verbosity.toUpperCase()) {
+      case "ALL":
+        return Level.ALL;
+      case "DEBUG":
+        return Level.DEBUG;
+      case "INFO":
+        return Level.INFO;
+      case "WARN":
+        return Level.WARN;
+      case "ERROR":
+        return Level.ERROR;
+      default:
         return Level.INFO;
     }
+  }
 
 }
